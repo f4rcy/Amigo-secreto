0a96e6658f635a48142048b587e9e8a87c9053ca
f4rcy <1012farcy@gmail.com>
2025-08-21 20:53:36 -0400

    actualizacion app.js

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombres=[];
+
+function agregarAmigo(){
+    let nombreAmigos = document.getElementById('amigo').value;
+
+    if (nombreAmigos=="") {
+        alert("Por favor inserta un nombre");
+    }else{
+        nombres.push(nombreAmigos);
+        mostraramigos();
+    }
+
+    console.log(nombres);
+
+    document.getElementById('amigo').value = "";
+
+    return; 
+}
+
+function mostraramigos(){
+    let lista=document.getElementById('listaAmigos');
+    lista.innerHTML = "";
+
+    for (let i=0; i<nombres.length; i++) {
+        lista.innerHTML = lista.innerHTML + "<li>" + nombres[i] + "</li>";
+    }
+
+    return;
+}
+
